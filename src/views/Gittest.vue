66adb012f2241a561be62fd33222c14f5e2d455f
jeonghee <toto_min4016@naver.com>
2021-04-20 12:00:09 +0900

    first commit

---
+++ src/views/Gittest.vue
@@ -0,0 +1,16 @@
+<template>
+  <Testview/>
+</template>
+
+<script>
+import Testview from '../components/Test'
+export default {
+    components:{
+        Testview
+    }
+}
+</script>
+
+<style>
+
+</style>
